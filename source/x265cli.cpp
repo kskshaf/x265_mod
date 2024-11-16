16e23b41284a9401b07ddbf3d6b88c13cf598b33
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2024-11-17 01:03:41 +0800

    Param: tune from x_x

--- source/x265cli.cpp
+++ source/x265cli.cpp
@@ -109,10 +109,13 @@ namespace X265_NS {
         H0("\nPresets:\n");
         H0("-p/--preset <string>             Trade off performance for compression efficiency. Default medium\n");
         H0("                                 ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow, or placebo\n");
+        H0("                                 Or slowxx lol (xx as xx in cxx)\n");
         H0("-t/--tune <string>               Tune the settings for a particular type of source or situation:\n");
-        H0("                                 psnr, ssim, grain, zerolatency, fastdecode\n");
+        H0("                                 psnr, ssim, grain, zerolatency, fastdecode,\n");
+        H0("                                 f~ffffffffffffffffff(18), e~eeeeeeeeeeeee(13), (sane range is e3~f7),\n");
+        H0("                                 vq1~vq3(vq1 if no trailing number(i.e. --tune vq == --tune vq1)),\n");
         H0("                                 (mid bitrate anime) littlepox/lp, (slower) littlepox++/lp++,\n");
-        H0("                                 (high bitrate anime BD / film) vcb-s/vcbs,   (slower) vcb-s++/vcbs++,\n");
+        H0("                                 (high bitrate anime BD / film) vcb-s/vcbs,   (slower) vcb-s++/vcbs++\n");
         H0("\nQuad-Tree size and depth:\n");
         H0("-s/--ctu <64|32|16>              Maximum CU size (WxH). Default %d\n", param->maxCUSize);
         H0("   --min-cu-size <64|32|16|8>    Minimum CU size (WxH). Default %d\n", param->minCUSize);
